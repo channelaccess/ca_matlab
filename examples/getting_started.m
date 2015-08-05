0405f1c887d6e799d45bff50b6d0ac68ca3731e5
ebner <simon.ebner@psi.ch>
2015-08-05 13:47:06 +0200

    added example folder

---
+++ examples/getting_started.m
@@ -0,0 +1,24 @@
+% This code snippet prints out the current ring current of SLS
+
+% Import the library
+% TODO: Remember to copy the ca_matlab library into the directory containing this file
+if not(exist('java_classpath_set'))
+    javaaddpath('ca_matlab-1.0.0.jar')
+    java_classpath_set=1
+end
+
+
+import ch.psi.jcae.*
+
+% Use of SLS configuration
+properties = java.lang.Properties()
+properties.setProperty('EPICS_CA_ADDR_LIST', 'sls-cagw')
+properties.setProperty('EPICS_CA_SERVER_PORT', '5062')
+
+context = Context(properties)
+channel = context.createChannel(ChannelDescriptor('double', 'YOUR-CHANNEL'))
+
+print channel.get()
+
+channel.close()
+context.close()
